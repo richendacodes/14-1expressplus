d06beca6810c3224808c12f198608b274e00ef62
Your Name <you@example.com>
2016-02-08 19:09:45 -0500

    And now it outputs

--- server.js
+++ server.js
@@ -15,25 +15,25 @@ var app = express();
 app.get('/add/:val1/:val2', function(req, res) {
   var total = parseInt(req.params.val1) + parseInt(req.params.val2);
   console.log(total);
-  res.send(total);
+  res.send(total+'');
 });
 
 app.get('/sub/:val1/:val2', function(req, res) {
   var total = parseInt(req.params.val1) - parseInt(req.params.val2);
   console.log(total);
-  res.send(total);
+  res.send(total+'');
 });
 
 app.get('/mult/:val1/:val2', function(req, res) {
   var total = parseInt(req.params.val1) * parseInt(req.params.val2);
   console.log(total);
-  res.send(total);
+  res.send(total+'');
 });
 
 app.get('/div/:val1/:val2', function(req, res) {
   var total = parseInt(req.params.val1) / parseInt(req.params.val2);
   console.log(total);
-  res.send(total);
+  res.send(total+'');
 });
 
 
